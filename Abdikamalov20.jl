88e9dd51f8f143a0aa40f814f70683c1d2702bd7
wiktoriatarnopolska <115220668+wiktoriatarnopolska@users.noreply.github.com>
2023-09-12 16:22:47 +0100

    Update Abdikamalov20.jl
    spike at \alpha_{13}, a = 0.9 for 30% Eddington ratio

--- Abdikamalov20.jl
+++ Abdikamalov20.jl
@@ -97,21 +97,21 @@ plot!(bins, edrat10, label = "Eddington ratio 10%")
 plot!(bins, edrat20, label = "Eddington ratio 20%")
 plot!(bins, edrat30, label = "Eddington ratio 30%")
 
-m2 = JohannsenMetric(M=1.0, a = 0.9, α13 = 0.0, ϵ3 = 0.0)
+m2 = JohannsenMetric(M=1.0, a = 0.9, α13 = 0.0, ϵ3 = 0.0) # spike at eddington ratio 30%
 
 d01 = ShakuraSunyaev(m2, eddington_ratio = 0.1)
 d02 = ShakuraSunyaev(m2, eddington_ratio = 0.2)
-d03 = ShakuraSunyaev(m2, eddington_ratio = 0.3)
+d03 = ShakuraSunyaev(m2, eddington_ratio = 0.3) #
 
 edrat0 = calculate_line_profile(m2, x, d_gt, bins, plane)
 edrat10 = calculate_line_profile(m2, x, d01, bins, plane)
 edrat20 = calculate_line_profile(m2, x, d02, bins, plane)
-edrat30 = calculate_line_profile(m2, x, d03, bins, plane)
+edrat30 = calculate_line_profile(m2, x, d03, bins, plane) #
 
 plot(bins, edrat0, label = "Eddington ratio 0%", title = "α13 = 0.0, a = 0.9", legend=:topleft)
 plot!(bins, edrat10, label = "Eddington ratio 10%")
 plot!(bins, edrat20, label = "Eddington ratio 20%")
-plot!(bins, edrat30, label = "Eddington ratio 30%")
+plot!(bins, edrat30, label = "Eddington ratio 30%") #
 
 m3 = JohannsenMetric(M=1.0, a = 0.998, α13 = 0.0, ϵ3 = 0.0)
 
